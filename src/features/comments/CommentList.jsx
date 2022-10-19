32c0dea4793a4124e592948d2716387d0272dfaa
wonstruckk <wien0526@gmail.com>
2022-10-19 13:39:31 +0900

    merging

--- src/features/comments/CommentList.jsx
+++ src/features/comments/CommentList.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
+import { __getComments } from '../../redux/modules/commentListSlice'
+
+import Comment from '../comments/Comment'
 
 const CommentList = () => {
+    const { id } = useParams
+    const dispatch = useDispatch()
+    const { comments } = useSelector(state => state.commentList)
+    console.log("cm", comments)
+    console.log("id", id)
+
+
+    useEffect(() => {
+        dispatch(__getComments())
+    }, [dispatch])
+
     return (
-        <div>CommentList</div>
+        <div>
+            {comments.map((comment) => comment.todoId === Number(id) ? <Comment key={comment.id} comment={comment}>댓글</Comment> : "")}
+        </div>
     )
 }
 
